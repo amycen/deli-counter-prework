3793ebe7cd0417d3057899391c6c3eb0045598f4
Amy Cen <amy.cen@blackrock.com>
2019-04-14 06:27:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,8 @@ def line(line)
   if line.length == 0
     puts "The line is currently empty."
   else
-    line.each_with_index do |name, index|
-      line_list += "#{index + 1}. #{name}"
-      if index < line.length - 1
-        line_list += ","
-      end
-    end
-  end
+    for i = 0 in line
+      line_list += "#{i + 1}. line[i]"
   binding.pry
   line_list = "The line is currently: #{line_list}."
 end
