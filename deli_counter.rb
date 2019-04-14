8227d75c77414f8daa2b8ecd0b0dfa5ddf7f4a55
Amy Cen <amy.cen@blackrock.com>
2019-04-14 06:17:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(line)
   else
 
     line.each_with_index do |name, index|
-      line_list += "#{index}. #{name}"
+      line_list += "#{index + 1}. #{name}"
       if index < line.length - 1
         line_list += ","
     line_list = "The line is currently: #{line_list}."
