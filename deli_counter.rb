e261b1c961428543e44ec81c3aebb18755235d82
Amy Cen <amy.cen@blackrock.com>
2019-04-14 06:21:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,13 @@ def line(line)
   if line.length == 0
     puts "The line is currently empty."
   else
-
     line.each_with_index do |name, index|
       line_list += "#{index + 1}. #{name}"
       if index < line.length - 1
         line_list += ","
       end
     end
+  end
     line_list = "The line is currently: #{line_list}."
 end
 
